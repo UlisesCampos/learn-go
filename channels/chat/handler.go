b608405ff5b4c663c1557826a414a4f33ef57013
ulises <ulisescamps9@gmail.com>
2020-12-29 20:50:16 -0600

    :fire:build the client
    Signed-off-by: ulises <ulisescamps9@gmail.com>

--- channels/chat/handler.go
+++ channels/chat/handler.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 
@@ -18,6 +19,10 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 	defer ws.Close()
 	clients[ws] = true
 
+	// if it's zero, no messages were ever sent/saved
+	if rdb.Exists("chat_messages").Val() != 0 {
+		sendPreviousMessages(ws)
+	}
 	for {
 		var msg models.ChatMessage
 		// Read in a new message as JSON and map it to a Message object
@@ -43,19 +48,24 @@ func sendPreviousMessages(ws *websocket.Conn) {
 		var msg models.ChatMessage
 		json.Unmarshal([]byte(chatMessage), &msg)
 		err := client.WriteJSON(msg)
-		if err != nil && unsafeError(err) {
-			log.Printf("error: %v", err)
-			client.Close()
-			delete(clients, client)
-		}
+		messageClient(ws, msg)
 	}
+}
+// If a message is sent while a client is clousing, ignore the error
+func unsafeError(err error) bool {
+	return !websocket.IsCloseError(err, websocket.CloseGoingAway) && err != io.EOF
 }
 func handleMessages() {
 	for {
 		// grab any next message from channel
 		msg := <-broadcaster
+
+		storeInRedis(msg)
+		messageClients(msg)
 	}
 }
+
+func
 
 func messageClients(msg models.ChatMessage) {
 	// send to every client currently connected
--- channels/chat/main.go
+++ channels/chat/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+
 	"github.com/Neil-uli/go-play/channels/chat/models"
 	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
@@ -17,8 +18,10 @@ var rdb *redis.Client
 // WS
 // clients is a list of all the currently active clients (or open WebSockets)
 var clients = make(map[*websocket.Conn]bool)
+
 // broadcaster is a single channel which is responsible for sending and receiving our ChatMessage data structure
 var broadcaster = make(chan models.ChatMessage)
+
 // upgrader is a bit of a clunker; it's necessary to "upgrade" Gorilla incoming requests into a WebSocket connection
 var upgrader = websocket.Upgrader{
 	// CheckOrigin returns true if the request Origin header is acceptable.
@@ -35,10 +38,6 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
-	r := mux.NewRouter()
-	// Serving static files
-	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./public")))
-	
 
 	// Redis client connection
 	redisURL := os.Getenv("REDIS_URL")
@@ -47,10 +46,14 @@ func main() {
 		panic(err)
 	}
 	rdb = redis.NewClient(opt)
-	
-	// WebSocket Connection 
+
+	r := mux.NewRouter()
+	// Serving static files
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./public")))
+
+	// WebSocket Connection
 	r.HandleFunc("/websocket", handleConnections)
-	go handleMessages
+	go handleMessages()
 
 	// Listen and start the server
 	log.Print("Server starting  at localhost:" + port)
