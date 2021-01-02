0b7dbfd22b717e35692454c7809e868bd31f1e61
ulises <ulisescamps9@gmail.com>
2021-01-02 15:03:21 -0600

    performing concurrent scanner

--- tcp-scanner/main.go
+++ tcp-scanner/main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"net"
+  "fmt"
+  "net"
+  "sync"
 )
 
 func main() {
-	for i := 1; i <= 1024; i++ {
-		address := fmt.Sprintf("scanme.nmap.org:%d", i)
-		conn, err := net.Dial("tcp", address)
-		if err != nil {
-			// port is closed of filtered
-			continue
-		}
-		conn.Close()
-		fmt.Printf("%d open\n", i)
-	}
+  var wg sync.WaitGroup
+  for i := 1; i <= 1024; i++ {
+    wg.Add(1)
+    go func(j int) {
+      defer wg.Done()
+      address := fmt.Sprintf("scanme.nmap.org:%d", j)
+      conn, err := net.Dial("tcp", address)
+      if err != nil {
+        return
+      }
+      conn.Close()
+      fmt.Printf("%d open\n", j)
+    }(i)
+  }
+  wg.Wait()
 }
